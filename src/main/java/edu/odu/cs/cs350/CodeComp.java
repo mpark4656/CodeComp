9f213822531612a9ad5a30c8d1d11a16cdbfa82e
mpark <mpark@cs.odu.edu>
2017-03-14 16:22:54 -0400

    Organized directories

--- src/main/java/edu/odu/cs/cs350/CodeComp.java
+++ src/main/java/edu/odu/cs/cs350/CodeComp.java
@@ -1,11 +1,9 @@
 package edu.odu.cs.cs350;
-import java.util.*;
 
 public class CodeComp
 {
 	public static void main( String[] args )
 	{
-		System.out.print( "CodeComp" );
+		System.out.println( "CodeComp" );
 	}
-	
 }
