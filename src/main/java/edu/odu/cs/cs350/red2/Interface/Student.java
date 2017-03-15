4c1ee70738857e04632cce2fe337db61431e561a
mpark <mpark@cs.odu.edu>
2017-03-15 16:48:05 -0400

    Started on Student class

--- src/main/java/edu/odu/cs/cs350/red2/Interface/Student.java
+++ src/main/java/edu/odu/cs/cs350/red2/Interface/Student.java
@@ -1,6 +1,29 @@
 package edu.odu.cs.cs350.red2.Interface;
 
+import java.util.ArrayList;
+
+/**
+ * Student Class -
+ * 
+ * @author mpark
+ */
 public class Student
 {
+	// This unique identifier is the Submission directory name minus the version ID
+	private String identifier;
+	
+	// All submissions that belong to this student
+	private ArrayList<Submission> submissions;
+	
+	/**
+	 * Constructor 
+	 * Instantiates Student with the given identifier
+	 * @param identifier
+	 */
+	public Student( String identifier )
+	{
+		
+	}
+	
 	
 }
