5f8a154883bde2c8ea96ba16ea0f811369f2cb29
mpark <mpark@cs.odu.edu>
2017-03-29 16:17:47 -0400

    Started Creating Interface for Token, TokenSequence, and TokenTypes enum

---
+++ src/main/java/edu/odu/cs/cs350/red2/LexicalTools/TokenSequence.java
@@ -0,0 +1,12 @@
+package edu.odu.cs.cs350.red2.LexicalTools;
+
+/**
+ * TokenSequenbce Class, 
+ * Stores Token objects
+ * @author mpark
+ *
+ */
+public class TokenSequence
+{
+	
+}
--- src/main/java/edu/odu/cs/cs350/red2/LexicalTools/TokenTypes.java
+++ src/main/java/edu/odu/cs/cs350/red2/LexicalTools/TokenTypes.java
@@ -1,10 +1,44 @@
 package edu.odu.cs.cs350.red2.LexicalTools;
 
+/**
+ * Enumeration for Token Types, 
+ * Currently supported types are 
+ * Keyword, 
+ * Integer Literal, 
+ * Identifier, 
+ * Operator, 
+ * EOF
+ * 
+ * @author mpark
+ *
+ */
 public enum TokenTypes
 {
+	/**
+	 * Java Keywords, 
+	 * Examples: int, String, if
+	 */
 	KEYWORD,
+	
+	/**
+	 * Any integer values
+	 */
 	INTEGER_LITERAL,
+	
+	/**
+	 * Any string literals not enclosed by ""
+	 */
 	IDENTIFIER,
+	
+	/**
+	 * Java Operators
+	 * Examples: +, -, () , []
+	 */
 	OPERATOR,
+	
+	/**
+	 * End of File
+	 */
 	EOF
+	
 }
