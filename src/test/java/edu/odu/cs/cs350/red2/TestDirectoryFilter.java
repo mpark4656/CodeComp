5818b507de8506993afd4699ac4b3b0b99b859aa
nruf <nruf001@odu.edu>
2017-04-02 06:42:16 -0400

    testing

---
+++ src/test/java/edu/odu/cs/cs350/red2/TestDirectoryFilter.java
@@ -0,0 +1,30 @@
+package edu.odu.cs.cs350.red2;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import edu.odu.cs.cs350.red2.FileFilter.DirectoryFilter;
+
+public class TestDirectoryFilter {
+
+	@Test
+	public void testDirectoryFilter() 
+	{
+		//initializing directoryFilter
+		DirectoryFilter filter = new DirectoryFilter( );
+		filter = new DirectoryFilter( );
+		
+		//first testcase
+		assertFalse( filter.accept(null) );
+		
+		//second testcase
+		assertTrue(filter.accept(null));
+		
+		
+		
+	}
+
+}
